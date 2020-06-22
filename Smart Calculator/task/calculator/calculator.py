292332ca62d4809a4ff36f702ac5986bab8b3ab2
llomellamomario <llomellamomario@gmail.com>
2020-06-23 00:09:49 +0200

    Second stage completed

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,3 +1,11 @@
 # write your code here
-x, y = map(int, input().split(' '))
-print(x + y)
+user_input = True
+while user_input != '/exit':
+    user_input = input()
+    if user_input != '/exit':
+        values = list(map(int, user_input.split()))
+        if len(values) == 1:
+            print(values[0])
+        elif len(values) == 2:
+            print(values[0] + values[1])
+print('Bye!')
