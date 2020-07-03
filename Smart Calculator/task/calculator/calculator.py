6c1d19c4803eafa890a2bdb6b27b8d5c4e7bd2c0
llomellamomario <llomellamomario@gmail.com>
2020-07-03 12:02:17 +0200

    Initial refactoring

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,16 +1,37 @@
 # write your code here
-user_input = True
-while user_input != '/exit':
-    user_input = input()
-    if user_input == '/help':
-        print('The program calculates the sum of numbers')
-    elif user_input == '':
+
+class Calculator:
+
+    def __init__(self, user_input=None):
+        self.user_input = user_input
+
+    def messages(self):
         pass
-    elif user_input != '/exit':
-        values = list(map(int, user_input.split(' ')))
-        result = 0
-        for number in values:
-            result += number
-        print(result)
-    else:
-        print('Bye!')
+
+    def logic(self, input):
+        self.user_input = input
+        if self.user_input == '/help':
+            print('The program calculates the sum of numbers')
+        elif self.user_input == '':
+            pass
+        elif self.user_input != '/exit':
+            values = list(self.user_input.split(' '))
+            result = 0
+            for number in values:
+                if number == '+':
+                    pass
+                else:
+                    result += int(number)
+            print(result)
+        else:
+            print('Bye!')
+
+    def simplify_operations(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    calc = Calculator()
+    while calc.user_input != '/exit':
+        calc.logic(input())
