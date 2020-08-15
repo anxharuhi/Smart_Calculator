38b9860fd4009cffe6b8e0b855a028e25995029e
llomellamomario <llomellamomario@gmail.com>
2020-08-15 19:51:52 +0200

    Substractions added. Code should not have too many boneheaded decisions to add other operations down the line

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -9,31 +9,56 @@ class Calculator:
     def messages(self):
         pass
 
-    def logic(self, input):
-        self.user_input = input
+    def __operation_list(self):
+        return self.user_input.split(' ')
+        # We can add later parenthesis by checking for them and converting
+        # whatever it is between them to a list occupying that position
+
+    def __calculate_sign(self, sign_string: str):
+        sign_list = list(sign_string)
+        sign = sign_list.pop()
+        for signs in sign_list:
+            if signs == '-' and sign == '+':
+                sign = '-'
+            elif signs == '-' and sign == '-':
+                sign = '+'
+        return sign
+
+    def __solve(self, op_list: list):
+        while len(op_list) > 1:
+            if len(op_list) < 1:
+                raise Warning('List of operations is smaller than 1 or Null') # Just a sanity check
+            else:
+                    sub_op = []
+                    sub_op.append(int(op_list.pop())) # First and last from the operation are numbers, so typecast them
+                    sub_op.append(op_list.pop()) # Get the first number and operand
+                    while not sub_op[-1].isnumeric():
+                        sub_op.append(op_list.pop()) # Then keep popping until you find anouther number
+                    sub_op[-1] = int(sub_op[-1]) # Same deal here
+                    if len(sub_op[1]) > 1:
+                        sub_op = [sub_op[0], self.__calculate_sign(sub_op[1]), sub_op[-1]] # Calculate the sign of the operation
+                    if sub_op[1] == '+':
+                        op_list.append(sub_op[-1] + sub_op[0])
+                    elif sub_op[1] == '-':
+                        op_list.append(sub_op[0] - sub_op[-1])
+        return op_list[0]
+
+    def logic(self):
         if self.user_input == '':
             pass
         elif self.user_input in self.commands.keys():
             print(self.commands[self.user_input])
         elif self.user_input != '/exit':
-            values = list(self.user_input.split(' '))
-            result = 0
-            for number in values:
-                if number == '+':
-                    pass
-                else:
-                    try:
-                        result += int(number)
-                    except:
-                        result = None
+            operation_list = list(self.__operation_list())
+            operation_list.reverse()  # Invert the list so we start at the beginning
+            result = self.__solve(operation_list)
             if result is not None:
                 print(result)
-
-    def simplify_operations(self):
-        pass
 
 
 if __name__ == '__main__':
     calc = Calculator()
     while calc.user_input != '/exit':
-        calc.logic(input())
+        calc.user_input = input()
+        # calc.user_input = '3 + 2 + 1'
+        calc.logic()
